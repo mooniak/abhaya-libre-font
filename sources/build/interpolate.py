2895e3445ee080d9aead641b7fd52ab52e56b2a2
Pathum Egodawatta <pathum@mooniak.com>
2015-07-31 15:36:04 +0530

    Adding build files

---
+++ sources/build/interpolate.py
@@ -0,0 +1,23 @@
+import sys, os
+
+def interpolate( font1, font2, fileName ):
+    file = open( fileName, 'r' )
+
+    for line in file:
+        fontInfo = [ x.strip() for x in line.split( "," ) ]
+        
+        os.system( "./interpolate.pe " + font1 + " "
+                   + font2 + " " + "\"" + fontInfo[0]
+                   + "\"" + " " + "\"" + fontInfo[1] + "\""
+                   + " " + "\""  + fontInfo[2] + "\"" 
+                   +  "  "  + "\"" + fontInfo[3] + "\""
+                   + " " + fontInfo[4]  )
+        
+def main( argv ):
+    try:
+        interpolate( argv[1], argv[2], argv[3] )
+    except:
+        interpolate( argv[1], argv[2], "weights" )
+
+if __name__ == "__main__":
+    main( sys.argv )
