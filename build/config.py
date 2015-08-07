71806a42ed4131dd1a54112390997d55701141ca
Pathum Egodawatta <pathumego@gmail.com>
2015-08-07 16:18:02 +0530

    Revert "removing build from git"
    This reverts commit 7b6f03f9a369b7c8bb12b2798f39312f2a99bf49.

---
+++ build/config.py
@@ -0,0 +1,24 @@
+FAMILY_NAME = 'AbhayaLibre'
+
+STYLE_NAMES = [
+    'Regular',
+    'Medium',
+    'SemiBold',
+    'Bold',
+    'ExtraBold',
+]
+
+UFOIG_ARGS = [
+    #'-kern',
+    #'-mark',
+    #'-hint',
+    '-flat',
+    #'-mkmk',
+    #'-clas',
+    #'-indi',
+]
+
+MAKEOTF_ARGS = [
+    '-r',
+    '-shw',
+]
