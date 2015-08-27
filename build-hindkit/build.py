e49ea27fd05ca3510ab7d5e3d01b0931b7d39e10
Liang Hai <lianghai@gmail.com>
2015-08-27 15:23:31 +0800

    Setup build.py

--- build-hindkit/build.py
+++ build-hindkit/build.py
@@ -6,27 +6,39 @@ kit.confirm_version('0.1.2')
 # - - -
 
 family = kit.Family(
-    trademark = 'Hind',
-    script = 'Bangla',
-    # hide_script_name = True,
+    trademark = 'Abhaya Libre',
+    script = 'Sinhala',
+    hide_script_name = True,
 )
 
 family.set_masters(
     modules = [
-        'kerning',
-        'mark_positioning',
-        'mark_to_mark_positioning',
+        # 'kerning',
+        # 'mark_positioning',
+        # 'mark_to_mark_positioning',
         # 'devanagari_matra_i_variants',
     ],
 )
+family.masters[0]._file_name = 'AbhayaLibre-Regular.ufo'
+family.masters[1]._file_name = 'AbhayaLibre-ExtraBold.ufo'
 
-family.set_styles()
+family.set_styles([
+    ('Regular',     0.0, 400),
+    ('Medium',     18.6, 500),
+    ('SemiBold',   41.3, 600),
+    ('Bold',       67.9, 700),
+    ('ExtraBold', 100.0, 800),
+])
+
+# - - -
+
+family.output_name_affix = '{} FDK'
 
 # - - -
 
 builder = kit.Builder(family)
 
-builder.fontrevision = '1.000'
+builder.fontrevision = '0.100'
 
 builder.set_options([
 
@@ -36,7 +48,7 @@ builder.set_options([
 
     'makeinstances', #!
     'checkoutlines', #!
-    'autohint',      #!
+    # 'autohint',      #!
 
     'do_style_linking',
     'use_os_2_version_4',
