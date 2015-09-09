ed57bf7db6f63e32f6fba059e8a7c26eaeff0612
Pathum Egodawatta <pathumego@gmail.com>
2015-09-09 18:27:07 +0530

    Adding the sample page files

--- scripts/gen-masters.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd ../sources/
-
-fontforge ../scripts/fontconvert AbhayaLibre-Regular.sfd --ufo
-fontforge ../scripts/fontconvert AbhayaLibre-ExtraBold.sfd --ufo
-
-mv AbhayaLibre-Regular.ufo ../build/masters/AbhayaLibre_0.ufo
-mv AbhayaLibre-ExtraBold.ufo ../build/masters/AbhayaLibre_1.ufo
